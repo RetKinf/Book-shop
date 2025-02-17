7a28960c96636ae8b6d8501c3a2e4facc33b5f0d
RetKinf <retkinf@gmail.com>
2025-02-17 17:30:18 +0200

    do hw

---
+++ src/main/java/com/example/bookshop/service/impl/BookServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.bookshop.service.impl;
+
+import com.example.bookshop.model.Book;
+import com.example.bookshop.repository.BookRepository;
+import com.example.bookshop.service.BookService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
