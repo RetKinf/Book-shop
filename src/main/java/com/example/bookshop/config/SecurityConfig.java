dae830b5db2f94eb5e006d0f642090a5391b63f1
RetKinf <retkinf@gmail.com>
2025-02-27 16:26:21 +0200

    Implemented JWT token validation

--- src/main/java/com/example/bookshop/config/SecurityConfig.java
+++ src/main/java/com/example/bookshop/config/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.example.bookshop.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import com.example.bookshop.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,12 +16,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableMethodSecurity
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
     private final UserDetailsService userDetailsService;
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -47,7 +52,15 @@ public class SecurityConfig {
                         session -> session.sessionCreationPolicy(
                                 SessionCreationPolicy.STATELESS
                         ))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .userDetailsService(userDetailsService)
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- src/main/java/com/example/bookshop/controller/AuthController.java
+++ src/main/java/com/example/bookshop/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.example.bookshop.controller;
 
+import com.example.bookshop.dto.user.UserLoginRequestDto;
+import com.example.bookshop.dto.user.UserLoginResponseDto;
 import com.example.bookshop.dto.user.UserRegistrationRequestDto;
 import com.example.bookshop.dto.user.UserResponseDto;
 import com.example.bookshop.exception.RegistrationException;
+import com.example.bookshop.security.AuthService;
 import com.example.bookshop.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     private final UserService userService;
+    private final AuthService authService;
 
     @Operation(
             summary = "Register new user",
@@ -26,5 +30,14 @@ public class AuthController {
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto)
             throws RegistrationException {
         return userService.register(requestDto);
+    }
+
+    @Operation(
+            summary = "Login a user",
+            description = "This endpoint allows a user to log in by providing their credentials"
+    )
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
+        return authService.authenticate(requestDto);
     }
 }
